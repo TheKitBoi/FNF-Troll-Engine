389276702fa19b8c7ad73ca5d7156f3819ec13de
hd <tariq2007nl@gmail.com>
2021-05-03 15:55:19 +0200

    oops

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -130,7 +130,7 @@ class TitleState extends MusicBeatState
 			// music.loadStream(Paths.music('freakyMenu'));
 			// FlxG.sound.list.add(music);
 			// music.play();
-			var now = new Date();
+			var now = Date.now();
 			trace(now.getHours());
 			if(now.getHours() >= 18) {
 				FlxG.sound.playMusic(Paths.music('freakyNight'), 0); 
